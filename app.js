cb1eaa47adacd7407d59c082cee441d84a84e9f5
leeyichien <tdme.tdem@me.com>
2019-07-28 11:16:54 +0800

    add login using passport and express-session

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const app = express();
 const exphbs = require("express-handlebars");
 const bodyParser = require("body-parser");
 const methodOverride = require("method-override");
+const session = require("express-session");
+const passport = require("passport");
 
 // 載入 model
 const db = require("./models");
@@ -19,6 +21,22 @@ app.set("view engine", "handlebars");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
+
+app.use(
+  session({
+    secret: "todo_sequelize",
+    resave: "false",
+    saveUninitialized: "false"
+  })
+);
+// 使用 Passport - 要在「使用路由器」前面
+app.use(passport.initialize());
+app.use(passport.session());
+require("./config/passport")(passport);
+app.use((req, res, next) => {
+  res.locals.user = req.user;
+  next();
+});
 
 // 使用路由器
 app.use("/users", require("./routes/user"));
